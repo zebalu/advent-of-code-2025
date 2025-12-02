47d06927a7c4c93bc62c96dce890effdcb7fb56e
Balázs Zaicsek <zebalu@gmail.com>
2025-12-02 06:44:13 +0100

    minimal refactor

--- Day02.java
+++ Day02.java
@@ -1,38 +1,38 @@
 void main() {
-    var inputLines = readInput();
-    var ranges = Arrays.stream(inputLines.getFirst().split(",")).map(Range::fromString).toList();
-    var sum = ranges.stream().map(this::palindromsInRange).flatMap(List::stream).mapToLong(Long::longValue).sum();
-    IO.println(sum);
-    var sum2 = ranges.stream().map(this::invalidsInRange).flatMap(List::stream).mapToLong(Long::longValue).sum();
-    IO.println(sum2);
+    var ranges = Arrays.stream(readInput().getFirst().split(",")).map(Range::fromString).toList();
+    var part1 = ranges.stream().flatMap(r -> r.collectBy(this::isPalindrom).stream()).mapToLong(Long::longValue).sum();
+    IO.println(part1);
+    var part2 = ranges.stream().flatMap(r -> r.collectBy(this::isInvalid).stream()).mapToLong(Long::longValue).sum();
+    IO.println(part2);
 }
 
 record Range(long start, long end) {
+    List<Long> collectBy(Predicate<Long> predicate) {
+        List<Long> result = new ArrayList<>();
+        for (long L = start; L <= end; L++) {
+            if (predicate.test(L)) {
+                result.add(L);
+            }
+        }
+        return result;
+    }
+
     static Range fromString(String rng) {
         String[] split = rng.split("-");
         return new Range(Long.parseLong(split[0]), Long.parseLong(split[1]));
-    }
-    long size() {
-        return end - start+1;
     }
 }
 
 boolean isPalindrom(long num) {
     String str = Long.toString(num);
-    if(str.length()%2!=0) {
-        return false;
-    } else {
-        return str.substring(0, str.length()/2).equals(str.substring(str.length()/2));
-    }
+    return str.length() > 1 && isRepetitionsOnly(str.substring(0, str.length() / 2), str);
 }
 
 boolean isInvalid(long num) {
     String str = Long.toString(num);
-    for(int i=1; i<=str.length()/2; ++i) {
-        if(str.length()%i==0) {
-            if(isRepetitionsOnly(str.substring(0,i), str)){
-                return true;
-            }
+    for (int i = 1; i <= str.length() / 2; ++i) {
+        if (str.length() % i == 0 && isRepetitionsOnly(str.substring(0, i), str)) {
+            return true;
         }
     }
     return false;
@@ -40,33 +40,13 @@ boolean isInvalid(long num) {
 
 boolean isRepetitionsOnly(String prefix, String whole) {
     String current = whole;
-    while(!current.isEmpty()) {
-        if(!current.startsWith(prefix)) {
+    while (!current.isEmpty()) {
+        if (!current.startsWith(prefix)) {
             return false;
         }
         current = current.substring(prefix.length());
     }
     return true;
-}
-
-List<Long> palindromsInRange(Range range) {
-    List<Long> result = new ArrayList<>();
-    for(long L=range.start(); L<=range.end(); ++L) {
-        if(isPalindrom(L)) {
-            result.add(L);
-        }
-    }
-    return result;
-}
-
-List<Long> invalidsInRange(Range range) {
-    List<Long> result = new ArrayList<>();
-    for(long L=range.start(); L<=range.end(); ++L) {
-        if(isInvalid(L)) {
-            result.add(L);
-        }
-    }
-    return result;
 }
 
 List<String> readInput() {
