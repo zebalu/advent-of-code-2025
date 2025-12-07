dec634942c2410dd908aedda9fcd22fedd6935af
Balázs Zaicsek <zebalu@gmail.com>
2025-12-07 07:47:12 +0100

    Day 07 first cleanup

--- Day07.java
+++ Day07.java
@@ -1,99 +1,69 @@
 void main() {
-    var input = readInput();
-    int startPos = input.getFirst().indexOf("S");
-    int countSpliterHit = part1(startPos, input);
-    IO.println(countSpliterHit);
-    Map<Coord, Long> beamWays = new HashMap<>();
-    List<Integer> beams = new ArrayList<>(List.of(startPos));
-    beamWays.put(new Coord(startPos, 0), 1L);
-    for(int row = 1; row < input.size(); ++row) {
-        String level =  input.get(row);
-        SequencedSet<Integer> newBeams = new LinkedHashSet<>();
-        for(int beam: beams) {
-            Coord beamPos = new Coord(beam, row);
-            Coord beamPrev = new Coord(beam, row-1);
-            if(level.charAt(beam) == '.') {
-                newBeams.add(beam);
-                long prev = beamWays.get(beamPrev);
-                beamWays.compute(beamPos, (k,v)->{
-                    if(v==null) {
-                        return prev;
-                    } else {
-                        return v+prev;
-                    }
-                });
-            } else if(level.charAt(beam) == '^') {
-                int left = beam-1;
-                int right = beam+1;
-                long ways = beamWays.get(beamPrev);
-                if(left >=0) {
-                    newBeams.add(left);
-                    beamWays.compute(new Coord(left, row), (k,v)->{
-                        if(v==null) {
-                            return ways;
-                        } else {
-                            return v+ways;
-                        }
-                    });
-                }
-                if(right < level.length()) {
-                    newBeams.add(right);
-                    beamWays.compute(new Coord(right, row), (k,v)->{
-                        if(v==null) {
-                            return ways;
-                        } else {
-                            return v+ways;
-                        }
-                    });
-                }
-                ++countSpliterHit;
-            }
-        }
-        beams = new ArrayList<>(newBeams);
-    }
-    long[] l = beamWays.entrySet().stream().filter(e->e.getKey().y==input.size()-1).sorted(Comparator.comparing(Map.Entry::getKey)).mapToLong(e->e.getValue()).toArray();
-    long ways = beamWays.entrySet().stream().filter(e->e.getKey().y==input.size()-1).mapToLong(Map.Entry::getValue).sum();
-    IO.println(ways);
-    IO.println(Arrays.toString(l));
+    Map<Coord, Long> beamWays = creatPathMap(readInput());
+    IO.println(part1(beamWays));
+    IO.println(part2(beamWays));
 }
 
-private static int part1(int startPos, List<String> input) {
-    List<Integer> beams = new ArrayList<>(List.of(startPos));
-    int countSpliterHit = 0;
-    for(int row = 1; row < input.size(); ++row) {
-        String level =  input.get(row);
+long part1(Map<Coord, Long> beamWays) {
+    return beamWays.values().stream().filter(v -> v == -1L).count();
+}
+
+long part2(Map<Coord, Long> beamWays) {
+    int lastRow = beamWays.keySet().stream().mapToInt(Coord::y).max().orElseThrow();
+    return beamWays.entrySet().stream().filter(e -> e.getKey().y == lastRow).mapToLong(Map.Entry::getValue).sum();
+}
+
+Map<Coord, Long> creatPathMap(List<String> input) {
+    int startPos = input.getFirst().indexOf("S");
+    Map<Coord, Long> beamWays = new HashMap<>();
+    SequencedSet<Integer> beams = new LinkedHashSet<>(List.of(startPos));
+    beamWays.put(new Coord(startPos, 0), 1L);
+    for (int row = 1; row < input.size(); ++row) {
+        String level = input.get(row);
         SequencedSet<Integer> newBeams = new LinkedHashSet<>();
-        for(int beam: beams) {
-            if(level.charAt(beam) == '.') {
+        for (int beam : beams) {
+            Coord beamPos = new Coord(beam, row);
+            Coord beamPrev = new Coord(beam, row - 1);
+            if (level.charAt(beam) == '.') {
                 newBeams.add(beam);
-            } else if(level.charAt(beam) == '^') {
-                int left = beam-1;
-                int right = beam+1;
-                if(left >=0) {
+                long prev = beamWays.get(beamPrev);
+                update(beamWays, beamPos, prev);
+            } else if (level.charAt(beam) == '^') {
+                int left = beam - 1;
+                int right = beam + 1;
+                long ways = beamWays.get(beamPrev);
+                beamWays.put(beamPos, -1L);
+                if (left >= 0) {
                     newBeams.add(left);
+                    update(beamWays, new Coord(left, row), ways);
                 }
-                if(right < level.length()) {
+                if (right < level.length()) {
                     newBeams.add(right);
+                    update(beamWays, new Coord(right, row), ways);
                 }
-                ++countSpliterHit;
             }
         }
-        beams = new ArrayList<>(newBeams);
+        beams = newBeams;
     }
-    return countSpliterHit;
+    return beamWays;
+}
+
+void update(Map<Coord, Long> beamWays, Coord pos, long value) {
+    beamWays.compute(pos, (k, v) -> {
+        if (v == null) {
+            return value;
+        } else {
+            return v + value;
+        }
+    });
 }
 
 List<String> readInput() {
     try {
-        return Files.readAllLines(Path.of("input_07.txt"), StandardCharsets.UTF_8);
+        return Files.readAllLines(Path.of("example_07.txt"), StandardCharsets.UTF_8);
     } catch (IOException e) {
         throw new RuntimeException(e);
     }
 }
 
-record Coord(int x, int y) implements Comparable<Coord> {
-    @Override
-    public int compareTo(Coord o) {
-        return Comparator.comparingInt(Coord::x).thenComparingInt(Coord::y).compare(this, o);
-    }
-}
+record Coord(int x, int y) { }
